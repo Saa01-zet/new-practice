dcd95c5d7b06652bd6c2fa38c362900353d24b3a
Saa01-zet <ibadullaevv7766@icloud.com>
2026-04-08 00:54:09 +0300

    Change readme and main.py in master (will cause conflict)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 ﻿def main():
-    print("Hello from main")
+    print("Hello from MAIN BRANCH")
+    print("Version: 2.0.0 - MASTER changes")
+
+def deploy():
+    print("Deploying to production")
 
 if __name__ == "__main__":
     main()
+    deploy()
